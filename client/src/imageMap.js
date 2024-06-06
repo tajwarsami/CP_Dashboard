c5c190117c5d91e130a2052258a4fed9e01262a9
Tajwar Hossain <107741603+tajwarsami@users.noreply.github.com>
2024-06-06 23:21:29 +0600

    img added,app contest updated

--- client/src/imageMap.js
+++ client/src/imageMap.js
@@ -1,7 +1,26 @@
 const imageMap = {
     'codeforces.com': '/images/codeforces.png',
-    'cucup.ac': '/images/codechef.png'
+    'codechef.com': '/images/codechef.png',
+    'codingninjas.com/codestudio': '/images/codingninjas.png',
+    'yukicoder.me': '/images/yukicoder.png',
+    'toph.co': '/images/toph.jpeg',
+    'ctftime.org': '/images/ctftime.png',
+    'codeweekend.dev': '/images/codeweekend.png',
+    'ucup.ac': '/images/ucup.png',
+    'luogu.com.cn': '/images/luogu.jpeg',
+    'contest.yandex.ru/CYF': '/images/yandex.png',
+    'atcoder.jp': '/images/atcoder.jpeg',
+    'projecteuler.net': '/images/projecteuler.png',
+    'leetcode.com': '/images/leetcode.png',
+    'kep.uz': '/images/kep.jpeg',
+    //'dl.gsu.by': '/images/codeforces.png',
+    'geeksforgeeks.org': '/images/geeksforgeeks.png',
+    'codingame.com': '/images/codingame.png',
+    'techgig.com': '/images/techgig.png',
+    //'icfpconference.org': '/images/codeforces.png',
+    'usaco.org': '/images/usaco.png'
 
+    
     // Add more mappings
 };
 
